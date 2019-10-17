3008689f42a223c95d333990425f6f391c700f00
JMAPeters <jorn.peters@live.nl>
2019-10-17 16:44:43 +0200

    data types

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,13 @@ import Model
 import View
 
 import Graphics.Gloss.Interface.IO.Game
+import System.Random
 
 main :: IO ()
-main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
+main = do rng <- getStdGen
+        playIO (InWindow "Game" (400, 400) (0, 0)) -- Or FullScreen
               black            -- Background color
-              10               -- Frames per second
+              30               -- Frames per second
               initialState     -- Initial state
               view             -- View function
               input            -- Event function
--- src/Model.hs
+++ src/Model.hs
@@ -2,17 +2,47 @@
 --   which represent the state of the game
 module Model where
 
-data InfoToShow = ShowNothing
-                | ShowANumber Int
-                | ShowAChar   Char
-
 nO_SECS_BETWEEN_CYCLES :: Float
-nO_SECS_BETWEEN_CYCLES = 5
+nO_SECS_BETWEEN_CYCLES = 1
 
 data GameState = GameState {
-                   infoToShow  :: InfoToShow
+                   gameObjects :: GameObjects
+                 , state :: State
+                 , score :: Int
                  , elapsedTime :: Float
                  }
 
+data State = Start | InGame | Pause | End
+
+data GameObjects = GameObjects {
+                   player :: Player
+                 , enemies :: [Enemy]
+                 , bullets :: [Bullets]
+                 }
+
+data Player = Player {
+               PosX :: Int
+             , PosY :: Int`
+             , Speed :: Int
+             , Lives :: Int
+             }
+
+data Enemy = Enemy {
+               PosX :: Int
+             , PosY :: Int
+             , Speed :: Int
+             }
+
+data Bullet = Bullet {
+               PosX :: Int
+             , PosY :: Int
+             , Type :: Char
+}
+
 initialState :: GameState
-initialState = GameState ShowNothing 0
+initialState = GameState startobjects Start 0 0
+
+startObjects = GameObjects startPlayer [] []
+
+startPlayer :: Player
+startPlayer = Player 0 0 0 3
